51639084ee1889aed2fbdf8fa5a71f0fbe24cff1
m0003r <m0003r@gmail.com>
2014-12-08 01:27:43 +0300

    Tests fix

---
+++ Tests/NoteRulesTest/TestBase.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Compositor.Levels;
+using Compositor.Rules.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NoteRulesTest
+{
+    public class TestBase<T> where T: IDeniable, ITemporal
+    {
+        internal void Satisfy(Predicate<T> expected,
+            Predicate<double> expectedFreq,
+            FreqsDict freqs)
+        {
+            foreach (var kv in freqs)
+            {
+                var actual = (T)kv.Key;
+                if (!expected(actual)) continue;
+
+                var freq = kv.Value;
+                Console.WriteLine("{0} at {1} => {2}", actual, actual.TimeStart, freq);
+                if (actual.AppliedRules != null)
+                    foreach (var p in actual.AppliedRules)
+                        Console.WriteLine("\t{0}: {1}", p.Item1.GetType().Name, p.Item2);
+
+                Assert.IsTrue(expectedFreq(freq));
+            }
+        }
+
+        internal void IsAllowed(T expectedNote,
+            FreqsDict freqs)
+        {
+            Console.WriteLine("Is allowed {0} at {1}?", expectedNote, expectedNote.TimeStart);
+            IsAllowed(n => n.Equals(expectedNote), freqs);
+        }
+
+        internal void IsAllowed(Predicate<T> expected,
+            FreqsDict freqs)
+        {
+            var count = freqs.Count(kv => expected((T)kv.Key));
+            Assert.IsTrue(count > 0);
+            
+            Satisfy(expected, x => x >= 0.03, freqs);
+        }
+
+        internal void IsOneAllowed(Predicate<T> expected, FreqsDict freqs)
+        {
+            IsAllowed(expected, freqs);
+            IsDenied(n => !expected(n), freqs);
+        }
+
+        internal void IsOneAllowed(T expected, FreqsDict freqs)
+        {
+            IsOneAllowed(n => n.Equals(expected), freqs);
+        }
+
+        internal void IsDenied(Predicate<T> expected,
+            FreqsDict freqs)
+        {
+            Satisfy(expected, x => x < 0.03, freqs);
+        }
+    }
+}
--- Tests/TwoVoiceTest/Properties/AssemblyInfo.cs
+++ Tests/TwoVoiceTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication1")]
+[assembly: AssemblyTitle("TwoVoiceTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication1")]
+[assembly: AssemblyProduct("TwoVoiceTest")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7c8a004e-fdf3-48c8-a7d4-a82da7ac2dab")]
+[assembly: Guid("44b2ef74-7992-4561-92a7-155cd5068f32")]
 
 // Version information for an assembly consists of the following four values:
 //
